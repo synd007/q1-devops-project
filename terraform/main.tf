3a208e6de6d4b1352af95f78570dc54f83093725
johneyo <eyo.john.79@gmail.com>
2026-06-10 21:09:43 +0100

    add kubernetes manifests, pgadmin deployment and fix rds security group

--- terraform/main.tf
+++ terraform/main.tf
@@ -161,6 +161,14 @@ resource "aws_security_group" "rds" {
     security_groups = [aws_security_group.eks.id]
   }
 
+  ingress {
+    from_port       = 5432
+    to_port         = 5432
+    protocol        = "tcp"
+    security_groups = [aws_eks_cluster.main.vpc_config[0].cluster_security_group_id]
+  }
+
+
   egress {
     from_port   = 0
     to_port     = 0
@@ -238,7 +246,7 @@ resource "aws_iam_role_policy_attachment" "eks_ecr_policy" {
 resource "aws_eks_cluster" "main" {
   name     = "dp-cluster"
   role_arn = aws_iam_role.eks_cluster.arn
-  version  = "1.29"
+  version  = "1.30"
 
   vpc_config {
     subnet_ids         = [aws_subnet.pub1.id, aws_subnet.pub2.id]
